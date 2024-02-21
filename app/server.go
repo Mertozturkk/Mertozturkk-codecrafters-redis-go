2c4ae1a257d5c54c71361363bb3696b219d76d0b
Mert Öztürk <mert.ozturk@teleskop.app>
2024-02-21 16:20:40 +0300

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -2,25 +2,34 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
 	"net"
-	"os"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	l, err := net.Listen("tcp", "localhost:6379")
 	if err != nil {
-            fmt.Println("Failed to bind to port 6379")
-	    os.Exit(1)
+		fmt.Println("Error", err)
+		return
 	}
-	_, err = l.Accept()
-	if err != nil {
-	    fmt.Println("Error accepting connection: ", err.Error())
-	    os.Exit(1)
+
+	defer l.Close()
+	fmt.Println("Server listening on ", l.Addr().String())
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error", err)
+			continue
+		}
+		go handleConnection(conn)
 	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	msg := []byte("+PONG\r\n")
+
+	conn.Write(msg)
+
 }
