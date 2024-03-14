8d9630483b25c4d49c7a1b2621d9afbe33c77f5f
Mert Öztürk <mert.ozturk@teleskop.app>
2024-03-14 21:53:11 +0300

    replication stage1

--- app/server.go
+++ app/server.go
@@ -1,79 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"github.com/codecrafters-io/redis-starter-go/formatters"
-	"github.com/codecrafters-io/redis-starter-go/models"
+	"flag"
+	"github.com/codecrafters-io/redis-starter-go/connections"
 	"github.com/codecrafters-io/redis-starter-go/storage"
-	"net"
-	"strings"
 )
 
 func main() {
 	store := storage.NewStore("initialKey", "initialValue")
+	// use flag for port
+	port := flag.String("port", "6379", "port to listen on")
+	flag.Parse()
 
-	l, err := net.Listen("tcp", "localhost:6379")
-	if err != nil {
-		fmt.Println("Error", err)
-		return
-	}
-
-	defer l.Close()
-	fmt.Println("Server listening on ", l.Addr().String())
-
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error", err)
-			continue
-		}
-		go handleConnection(conn, store)
-	}
-}
-
-func handleConnection(conn net.Conn, store *storage.Store) {
-	defer conn.Close()
-
-	for {
-		buf := make([]byte, 2048)
-		n, err := conn.Read(buf)
-		if err != nil {
-			return
-		}
-		receiveMessage := string(buf[:n])
-
-		cliData, err := formatters.StringParser(receiveMessage)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		cmd := cliData.Command
-
-		switch cmd {
-		case models.Echo:
-			joinedData := strings.Join(cliData.Data, " ")
-			joinedData += "\r\n"
-			conn.Write([]byte("+" + joinedData))
-
-		case models.Ping:
-			conn.Write([]byte("+PONG\r\n"))
-
-		case models.Set:
-			err := store.Set(cliData.Data[0], cliData.Data[1], cliData.Timer)
-			if err != nil {
-				fmt.Println("Error:", err)
-				return
-			}
-			conn.Write([]byte("+OK\r\n"))
-
-		case models.Get:
-			value, ok := store.Get(cliData.Data[0])
-			if ok {
-				conn.Write([]byte("+" + value + "\r\n"))
-			} else {
-				conn.Write([]byte("$-1\r\n"))
-			}
-		}
-
-	}
+	manager := connections.NewNetManager(*port)
+	go manager.Init()
+	connections.StartNetManager(manager, store)
 }
---
+++ connections/connection.go
@@ -0,0 +1,141 @@
+package connections
+
+import (
+	"fmt"
+	"github.com/codecrafters-io/redis-starter-go/formatters"
+	"github.com/codecrafters-io/redis-starter-go/models"
+	"github.com/codecrafters-io/redis-starter-go/storage"
+	"net"
+	"strings"
+)
+
+type Client struct {
+	manager    *NetManager
+	connection net.Conn
+	send       chan []byte
+	message    chan []byte
+}
+
+func NewClient(manager *NetManager, connection net.Conn) *Client {
+	return &Client{
+		manager:    manager,
+		connection: connection,
+		send:       make(chan []byte),
+		message:    make(chan []byte),
+	}
+}
+
+type NetManager struct {
+	listener   *net.Listener
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+}
+
+func (manager *NetManager) Init() {
+
+	for {
+		select {
+		case conn := <-manager.register:
+			manager.clients[conn] = true
+		case conn := <-manager.unregister:
+			if _, ok := manager.clients[conn]; ok {
+				close(conn.send)
+				delete(manager.clients, conn)
+			}
+		case message := <-manager.broadcast:
+			for conn := range manager.clients {
+				select {
+				case conn.send <- message:
+				default:
+					close(conn.send)
+					delete(manager.clients, conn)
+				}
+			}
+		}
+
+	}
+}
+
+func NewNetManager(port string) *NetManager {
+	manager := &NetManager{
+		clients:    make(map[*Client]bool),
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+	}
+
+	listener, err := net.Listen("tcp", "localhost:"+port)
+
+	if err != nil {
+		fmt.Println("Error", err)
+		return nil
+	}
+
+	manager.listener = &listener
+
+	return manager
+}
+
+func StartNetManager(manager *NetManager, store *storage.Store) {
+
+	defer (*manager.listener).Close()
+	fmt.Println("Server listening on ", (*manager.listener).Addr().String())
+
+	for {
+		conn, err := (*manager.listener).Accept()
+		if err != nil {
+			fmt.Println("Error", err)
+			continue
+		}
+		client := NewClient(manager, conn)
+		manager.register <- client
+		go HandleConnection(client, store)
+	}
+}
+
+func HandleConnection(client *Client, store *storage.Store) {
+	defer client.connection.Close()
+
+	for {
+		buf := make([]byte, 2048)
+		n, err := client.connection.Read(buf)
+		if err != nil {
+			return
+		}
+		receiveMessage := string(buf[:n])
+
+		cliData, err := formatters.StringParser(receiveMessage)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		cmd := cliData.Command
+
+		switch cmd {
+		case models.Echo:
+			joinedData := strings.Join(cliData.Data, " ")
+			joinedData += "\r\n"
+			client.connection.Write([]byte("+" + joinedData))
+
+		case models.Ping:
+			client.connection.Write([]byte("+PONG\r\n"))
+
+		case models.Set:
+			err := store.Set(cliData.Data[0], cliData.Data[1], cliData.Timer)
+			if err != nil {
+				fmt.Println("Error:", err)
+				return
+			}
+			client.connection.Write([]byte("+OK\r\n"))
+		case models.Get:
+			value, ok := store.Get(cliData.Data[0])
+			if ok {
+				client.connection.Write([]byte("+" + value + "\r\n"))
+			} else {
+				client.connection.Write([]byte("$-1\r\n"))
+			}
+		}
+	}
+}
